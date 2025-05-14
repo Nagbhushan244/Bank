4550a9934efe635e7b90c4d84c168d801186aa42
Nagbhushan <01fe23bca244@kletech.ac.in>
2025-05-14 16:07:15 +0530

    save

---
+++ bank.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+class BankCustomer{
+    String cust_name;
+    String acc_num;
+    int age;
+
+    BankCustomer(String cname,String acc_num,int age){
+        this.cust_name = cname;
+        this.acc_num = acc_num;
+        this.age=age;
+    }
+
+
+    public void displayDetails(){
+        System.out.println("Customer Name: "+cust_name);
+        System.out.println("Account Number: "+acc_num);
+        System.out.println("Age: "+age);
+    }
+}
+
+public class bank {
+    public static void main(String[] args) {
+        BankCustomer b1 = new BankCustomer("Ram", "IDFC2022000111", 28);
+        b1.displayDetails();
+
+        System.out.println("");
+
+       
+        }
+
+}
